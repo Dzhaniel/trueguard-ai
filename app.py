da1b089d3ddd81c88cdf95c4cf261db8f1460308
Dzhaniel <zanelmejrambekkyzy@gmail.com>
2026-02-21 04:09:56 +0500

    fix deploy

--- app.py
+++ app.py
@@ -163,3 +163,9 @@ async def analyze(
     }
 
     return JSONResponse(result)
+    import os
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
